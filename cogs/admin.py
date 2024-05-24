ee151c3a30627ad208bac8b74c7f720c725399c4
Rhodney James Paraiso <rjparaiso369@gmail.com>
2024-05-24 16:22:09 +0100

    feat/Help-Command (Changes, Modification, Improvement) feat/Interaction-Commands (Changes)
    Config changes
      - Created embed generator
      - Set up the Admin cog
    
    Help Command Changes
      - Setted up the dictionaries for the content of each help page
      - Used paginator to create a multipage help command
      - Set up the help command
      - Finished setting up the help command
      - Help command now exists through `admin.py`
      - More General now
      - Created another ReturnEmbed() function that returns a dictionary with the title of the command page as a key, the embed as a value
    
    *Changes to*
      - cogs/admin.py
      - main.py
    
    *Added files/folder*
      - cogs (folder)
      - cogs/admin.py (file)
      - gif folder, to store .txt files containing links to the gifs needed
      - Added 10 gifs
      - Created kissGifs.txt, self explanatory
    
    Fixed:
      - merge conflicts
    
    Deleted:
      - Closed the feat/Interaction-Commands branch

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
     Aman Bot
+    Admin Commands
 """
 
 import sys, asyncio, functools, itertools, math, random, os, discord, aiohttp, io, json, pickle, string, random
@@ -12,6 +13,14 @@ from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions, CheckFailure
 from itertools import cycle
 
+#### Paginator
+import button_paginator as pg
+bot = commands.Bot(command_prefix="seb ", intents=discord.Intents.all(), description=DESCRIPTION, help_command=None)
+
+class Admin(commands.Cog):    
+    def __init__(self, bot):
+        self.bot = bot    
+
 bot = commands.Bot(command_prefix="seb ", intents=discord.Intents.all(), description=DESCRIPTION, help_command=None)
 
 class Admin(commands.Cog):    
@@ -21,3 +30,21 @@ class Admin(commands.Cog):
     @commands.command()
     async def cogtest(self, ctx):
         await ctx.send("Cogs working")
+
+    @commands.command()
+    async def help(self, ctx):
+        embeds = []
+
+        # embeds.append(EmbedGenerator())
+
+        allEmbeds = ReturnEmbed()
+
+        for v in allEmbeds.values():
+            embeds.append(v)
+
+        paginator = pg.Paginator(bot, embeds, ctx)
+        # paginator.default_pagination()
+        paginator.add_button("prev", emoji = "◀")
+        paginator.add_button("goto")
+        paginator.add_button("next", emoji = "▶")
+        await paginator.start()
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 """
+    Important imports
+"""
+import discord
+
+"""
     Basic String Configurations
 """
 
@@ -10,3 +15,57 @@ RANDOM_HALAL: list = [
     "Subhanallah look at the ummah today",
     "Kes omak ya khara"
 ]
+
+################################# FUN COMMAND CONFIG #################################
+with open("gifs/kissGifs.txt", "r") as f:
+    kiss = f.readlines()
+
+KISS_GIFS: list = [s.strip() for s in kiss]
+################################# HELP COMMAND CONFIG #################################
+ 
+"""
+    Help Command Configs
+    ############################## NOTE ##############################
+    Please, if you're going to add a command, do it here.
+    I've already made sure that the help command will run properly, just edit it here
+"""
+
+ADMIN_COMS: dict = {
+    "help" : "Self explanatory - Sample use ```seb help```",
+    "Kick" : "Self explanatory - Sample use ```seb kick @user```",
+}
+
+FUN_COMS: dict = {
+    "Kiss" : "Kiss a user - Sample use ```seb kiss @user```"
+}
+
+"""
+    Help Command Embed Generator
+    ############################## NOTE ##############################
+
+    If you're going to edit these, let me know first
+"""
+
+OUTPUT_COMS: dict = {
+    "Admin Commands" : ADMIN_COMS,
+    "Fun Commands" : FUN_COMS,
+}
+
+def EmbedGenerator(nameDict, embedDict): #### Returns a Discord Embed
+    embed = discord.Embed(
+        title = f"{nameDict}",
+        # description="Admin commands"
+    )
+    
+    # for k, v in embedDict.items():
+    [embed.add_field(name=k, value=v, inline=False) for k, v in embedDict.items()]
+
+    return embed
+
+def ReturnEmbed():
+    output = {}
+
+    for k, v in OUTPUT_COMS.items():
+        output[k] = EmbedGenerator(k, v)
+
+    return output
