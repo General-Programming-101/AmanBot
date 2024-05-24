d0218f587a45501736fa8a4696804e7274643400
Rhodney James Paraiso <rjparaiso369@gmail.com>
2024-05-24 18:13:04 +0100

    .

---
+++ cogs/fun.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+    Aman Bot
+    Fun Commands
+"""
+
+import sys, asyncio, functools, itertools, math, random, os, discord, aiohttp, io, json, pickle, string, random
+from config import *
+from dotenv import load_dotenv, find_dotenv
+from itertools import cycle
+from async_timeout import timeout
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions, CheckFailure
+from itertools import cycle
+
+#### Paginator
+import button_paginator as pg
+bot = commands.Bot(command_prefix="seb ", intents=discord.Intents.all(), description=DESCRIPTION, help_command=None)
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def kiss(self, ctx, arg=""):
+
+        if arg == "":
+            await ctx.send("You can't kiss yourself, degenerate. Well... go on choose someone")
+        
+        else:
+            embed = discord.Embed(
+                description="{} kisses {}".format(ctx.author.mention, arg)
+            )
+            # embed.set_image(url=random.choice(KISS_GIFS))
+            await ctx.send(embed=embed)
